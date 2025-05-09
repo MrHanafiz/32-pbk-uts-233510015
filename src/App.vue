906b77b535279751c1d7a59ac11b3f2d600fb1d7
MrHanafiz <marhan.daek12@gmail.com>
2025-05-09 18:32:18 +0700

    pembaruan App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,30 @@
 <template>
-<div class="app-container">
+  <div class="app-container">
     <h1>My To-Do-List</h1>
+    
     <form @submit.prevent="addTodo" class="todo-form">
       <input v-model="newTodo" placeholder="Bos, silahkan isi kegiatan anda" />
       <button type="submit">Tambah</button>
     </form>
+
     <div class="filter-buttons">
       <button :class="{ active: filter === 'all' }" @click="filter = 'all'">Semua</button>
       <button :class="{ active: filter === 'active' }" @click="filter = 'active'">Belum Selesai</button>
       <button :class="{ active: filter === 'done' }" @click="filter = 'done'">Sudah Selesai</button>
     </div>
+
     <ul class="todo-list">
       <li v-for="(todo, index) in filteredTodos" :key="index" class="todo-item">
         <label class="checkbox-label">
           <input type="checkbox" v-model="todo.done" />
           <span :class="{ done: todo.done }">{{ todo.text }}</span>
+        </label>
         <button class="delete-button" @click="removeTodo(index)">🗑</button>
       </li>
     </ul>
   </div>
-
 </template>
+
 
 <script setup>
 import { ref, computed } from 'vue'
